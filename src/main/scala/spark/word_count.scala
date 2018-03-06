094577215bc147db89c09f6258efeb6a183eef92
airztz <airztz@gmail.com>
2018-03-06 04:58:12 -0800

    Updated remote submit command line

--- src/main/scala/spark/word_count.scala
+++ src/main/scala/spark/word_count.scala
@@ -3,8 +3,20 @@ package spark
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 
+//submit inside cluster
 //spark-submit --class spark.word_count --master yarn --deploy-mode cluster Scala4fun-assembly-0.1.jar
+
+//submit outside a cluster
+//V1 log won't move to spark history server
 //spark-submit --class spark.word_count --master yarn --deploy-mode cluster --conf spark.yarn.jars=hdfs://Joy4funCluster/user/ec2-user/spark_jars/*.jar target/scala-2.11/Scala4fun-assembly-0.1.jar
+
+//V2 log will move to spark history server
+//spark-submit --class spark.word_count --master yarn --deploy-mode cluster --properties-file spark-defaults.conf target/scala-2.11/Scala4fun-assembly-0.1.jar
+
+//create  spark-defaults.conf
+//spark.eventLog.enabled            true
+//spark.eventLog.dir                hdfs://Joy4funCluster/user/spark/applicationHistory
+//spark.yarn.historyServer.address  hdfsHA5:18080
 
 object word_count {
   def main(args: Array[String]){
